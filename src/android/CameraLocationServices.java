b84b4fec306e90c4dcd24fef0fb3fcd5d4d52805
Alexandru Dimitriu <alexandru.dimitriu@vlinde.com>
2020-01-27 16:21:33 +0200

    aOS Motion Detection + GPS exif
    Code style & added exif GPS support
    
    
    Motion Detection + GPS exif
    
    
    Removed some comments
    
    
    Improved the camera for aOS
    
    
    Improvements
    
    
    IMPROVED aOS

---
+++ src/android/CameraLocationServices.java
@@ -0,0 +1,289 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+package com.cordovaplugincamerapreview;
+
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.gson.Gson;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CameraLocationServices implements GoogleApiClient.ConnectionCallbacks {
+
+    private CameraLocationListener mListener;
+    private boolean mWantLastLocation = false;
+    private boolean mWantUpdates = false;
+    private CallbackContext mCbContext;
+    private GApiUtils mGApiUtils;
+    private GoogleApiClient mGApiClient;
+    public CordovaInterface cordova;
+    public Location globalLocation;
+    CallbackContext callbackContext;
+
+    public CameraLocationServices(CordovaInterface cordova) {
+        Log.d(LocationUtils.APPTAG, "CameraLocationServices");
+        this.cordova = cordova;
+        mGApiClient = new GoogleApiClient.Builder(cordova.getActivity())
+                .addApi(LocationServices.API).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(getGApiUtils()).build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(LocationUtils.APPTAG, "Location Services connected");
+        if (mWantLastLocation) {
+            mWantLastLocation = false;
+            getLastLocation();
+        }
+
+        if (mListener != null && mWantUpdates) {
+            mWantUpdates = false;
+            mListener.start();
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(LocationUtils.APPTAG, "GoogleApiClient connection has been suspend");
+    }
+
+
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param callbackContext The callback id used when calling back into JavaScript.
+     * @return True if the action was valid, or false if not.
+     */
+    public boolean getLocation(final CallbackContext callbackContext) {
+        Log.d(LocationUtils.APPTAG, "getLocation");
+        this.callbackContext = callbackContext;
+
+
+        if (isGPSdisabled()) {
+            // There it was the photo save.
+            Log.d(LocationUtils.APPTAG, "The isGPSdisabled is: " + String.valueOf(isGPSdisabled()));
+        } else {
+            if (getGApiUtils().servicesConnected()) {
+                if (!mGApiClient.isConnected() && !mGApiClient.isConnecting()) {
+                    mGApiClient.connect();
+                }
+
+                if (mGApiClient.isConnected()) {
+                    getLastLocation(callbackContext);
+                } else {
+                    setWantLastLocation(callbackContext);
+                }
+
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Called when the activity is to be shut down. Stop listener.
+     */
+    public void onDestroy() {
+        if (mListener != null) {
+            mListener.destroy();
+        }
+
+        if (mGApiClient.isConnected() || mGApiClient.isConnecting()) {
+            // After disconnect() is called, the client is considered "dead".
+            mGApiClient.disconnect();
+        }
+    }
+
+    public void completeDestroy() {
+        clearWatch("1");
+        onDestroy();
+        mInstance = null;
+        mListener = null;
+
+    }
+
+    /**
+     * Called when the view navigates. Stop the listeners.
+     */
+    public void onReset() {
+        this.onDestroy();
+    }
+
+
+    /**
+     * Location failed. Send error back to JavaScript.
+     *
+     * @param code The error code
+     * @param msg  The error message
+     */
+    public void fail(int code, String msg, CallbackContext callbackContext, boolean keepCallback) {
+        Log.e(LocationUtils.APPTAG, "The location service failed!");
+    }
+
+    private boolean isGPSdisabled() {
+        boolean gps_enabled;
+
+        LocationManager lm = (LocationManager) this.cordova.getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            gps_enabled = false;
+        }
+
+        return !gps_enabled;
+    }
+
+    private void getLastLocation() {
+        getLastLocation(mCbContext);
+        mCbContext = null;
+    }
+
+    private void getLastLocation(CallbackContext callbackContext) {
+        int maximumAge = 100000;
+
+        try {
+
+            Location last = LocationServices.FusedLocationApi.getLastLocation(mGApiClient);
+
+            // Check if we can use lastKnownLocation to get a quick reading and use
+            // less battery
+
+            if (last != null && (System.currentTimeMillis() - last.getTime()) <= maximumAge) {
+                getGPSData(last);
+            } else {
+                getCurrentLocation(callbackContext, 5000);
+            }
+
+        } catch (Exception e) {
+            getCurrentLocation(callbackContext, 5000);
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setWantLastLocation(CallbackContext callbackContext) {
+        mCbContext = callbackContext;
+        mWantLastLocation = true;
+    }
+
+    private void getCurrentLocation(CallbackContext callbackContext, int timeout) {
+        getListener().addCallback(callbackContext, timeout);
+    }
+
+    private void clearWatch(String id) {
+        getListener().clearWatch(id);
+    }
+
+    private void addWatch(String timerId, CallbackContext callbackContext) {
+        getListener().addWatch(timerId, callbackContext);
+    }
+
+    private CameraLocationListener getListener() {
+        if (mListener == null) {
+            mListener = new CameraLocationListener(mGApiClient, this, LocationUtils.APPTAG);
+        }
+        return mListener;
+    }
+
+    private GApiUtils getGApiUtils() {
+        if (mGApiUtils == null) {
+            mGApiUtils = new GApiUtils(cordova);
+        }
+        return mGApiUtils;
+    }
+
+    /**
+     * The code GPS implementation.
+     * This code should be improved.
+     **/
+
+
+    public static String getDms(double val) {
+        double valDeg;
+        double valMin;
+        double valSec;
+        String result;
+
+        val = Math.abs(val);
+
+        valDeg = Math.floor(val);
+        result = String.valueOf((int) valDeg) + "/1,";
+
+        valMin = Math.floor((val - valDeg) * 60);
+        result += String.valueOf((int) valMin) + "/1,";
+
+        valSec = Math.round((val - valDeg - valMin / 60) * 3600 * 10000);
+        result += String.valueOf((int) valSec) + "/10000";
+
+        return result;
+    }
+
+    private static CameraLocationServices mInstance;
+    private List<Listener> listeners = new ArrayList<Listener>();
+
+    public static CameraLocationServices getInstance(CordovaInterface cordova, CallbackContext context) {
+        if (mInstance == null) {
+            mInstance = new CameraLocationServices(cordova);
+            mInstance.getLocation(context);
+        }
+
+        return mInstance;
+    }
+
+    public void addListener(Listener toAdd) {
+        listeners.add(toAdd);
+    }
+
+    public void getGPSData(Location location) {
+        globalLocation = location;
+
+        final int priority = LocationRequest.PRIORITY_HIGH_ACCURACY;
+        final long interval = LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS;
+        final long fastInterval = LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS;
+
+        getListener().setLocationRequestParams(priority, interval, fastInterval);
+        mWantUpdates = true;
+        addWatch("1", callbackContext);
+
+        for (Listener hl : listeners) {
+            hl.getCoordonates(location);
+        }
+    }
+
+    public interface Listener {
+        void getCoordonates(Location location);
+    }
+}
---
+++ src/android/MovementDetector.java
@@ -0,0 +1,156 @@
+package com.cordovaplugincamerapreview;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import org.apache.cordova.CordovaPlugin;
+
+import java.util.HashSet;
+import java.util.List;
+
+
+
+public class MovementDetector extends CordovaPlugin implements SensorEventListener {
+
+  protected final String TAG = getClass().getSimpleName();
+
+  private SensorManager sensorMan;
+  private Sensor accelerometer;
+
+  private float x, y;                                // most recent acceleration values
+  private int accuracy = SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM;
+  private int current_position;
+
+  private MovementDetector() {
+  }
+
+  private static MovementDetector mInstance;
+
+  public static MovementDetector getInstance(Context context) {
+    if (mInstance == null) {
+      mInstance = new MovementDetector();
+      mInstance.init(context);
+    }
+    return mInstance;
+  }
+
+  //////////////////////
+  private HashSet<Listener> mListeners = new HashSet<MovementDetector.Listener>();
+
+  private void init(Context context) {
+    sensorMan = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+    accelerometer = sensorMan.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+  }
+
+  public void start() {
+    // Get accelerometer from sensor manager
+    List<Sensor> list = this.sensorMan.getSensorList(Sensor.TYPE_ACCELEROMETER);
+
+    // If found, then register as listener
+    if ((list != null) && (list.size() > 0)) {
+      this.accelerometer = list.get(0);
+      if (this.sensorMan.registerListener(this, this.accelerometer, SensorManager.SENSOR_DELAY_NORMAL)) {
+        // CB-11531: Mark accuracy as 'reliable' - this is complementary to
+        // setting it to 'unreliable' 'stop' method
+        this.accuracy = SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM;
+      }
+
+    }
+  }
+
+
+  public void stop() {
+    try {
+      sensorMan.unregisterListener(this);
+    } catch (Exception e) {
+
+      Log.e(TAG, "Asta e cu eroare!");
+      Log.e(TAG, e.getMessage());
+    }
+  }
+
+  public void addListener(Listener listener) {
+    mListeners.add(listener);
+  }
+
+
+  public static final int UPSIDE_DOWN = 3;
+  public static final int LANDSCAPE_RIGHT = 4;
+  public static final int PORTRAIT = 1;
+  public static final int LANDSCAPE_LEFT = 2;
+  public int mOrientationDeg; //last rotation in degrees
+  public int mOrientationRounded; //last orientation int from above
+  private static final int _DATA_X = 0;
+  private static final int _DATA_Y = 1;
+  private static final int _DATA_Z = 2;
+  private int ORIENTATION_UNKNOWN = -1;
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    float[] values = event.values;
+    int orientation = ORIENTATION_UNKNOWN;
+    float X = -values[_DATA_X];
+    float Y = -values[_DATA_Y];
+    float Z = -values[_DATA_Z];
+    float magnitude = X * X + Y * Y;
+    int tempOrientRounded = 0;
+
+
+    // Don't trust the angle if the magnitude is small compared to the y value
+    if (magnitude * 4 >= Z * Z) {
+      float OneEightyOverPi = 57.29577957855f;
+      float angle = (float) Math.atan2(-Y, X) * OneEightyOverPi;
+      orientation = 90 - (int) Math.round(angle);
+      // normalize to 0 - 359 range
+      while (orientation >= 360) {
+        orientation -= 360;
+      }
+      while (orientation < 0) {
+        orientation += 360;
+      }
+    }
+    //^^ thanks to google for that code
+    //now we must figure out which orientation based on the degrees
+
+    if (orientation != mOrientationDeg) {
+      mOrientationDeg = orientation;
+      //figure out actual orientation
+      if (orientation == -1) {//basically flat
+        tempOrientRounded = 1;//portrait
+      } else if (orientation <= 45 || orientation > 315) {//round to 0
+        tempOrientRounded = 1;//portrait
+      } else if (orientation > 45 && orientation <= 135) {//round to 90
+        tempOrientRounded = 2; //lsleft
+      } else if (orientation > 135 && orientation <= 225) {//round to 180
+        tempOrientRounded = 3; //upside down
+      } else if (orientation > 225 && orientation <= 315) {//round to 270
+        tempOrientRounded = 4;//lsright
+      }
+    }
+
+    if (tempOrientRounded != 0 && mOrientationRounded != tempOrientRounded) {
+      //Orientation changed, handle the change here
+      mOrientationRounded = tempOrientRounded;
+      for (Listener listener : mListeners) {
+        listener.onMotionDetected((mOrientationRounded - 1) * 90);
+      }
+    }
+  }
+
+  /* (non-Javadoc)
+   * @see android.hardware.SensorEventListener#onAccuracyChanged(android.hardware.Sensor, int)
+   */
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public interface Listener {
+    void onMotionDetected(int postion);
+  }
+}
